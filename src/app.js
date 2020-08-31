332f1a48ed1c3b057a351dc44c59181c113a3de3
themsiqueira <mateussiqueiracarneiro@outlook.com>
2020-08-31 14:42:25 -0300

    Tasks now show user wallet ID and date of creation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+
 App = {
   loading: false,
   contracts: {},
+
   ///////////////////////////////// METAMASK INTEGRATION /////////////////////////////////
   load: async () => {
     await App.loadWeb3()
@@ -8,6 +10,7 @@ App = {
     await App.loadContract()
     await App.render()
   },
+
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
@@ -52,6 +55,7 @@ App = {
     const quotationContract = await $.getJSON('QuotationContract.json')
     App.contracts.QuotationContract = TruffleContract(quotationContract)
     App.contracts.QuotationContract.setProvider(App.web3Provider)
+
     // Hydrate the smart contract with values from the blockchain
     App.quotationContract = await App.contracts.QuotationContract.deployed()
   },
@@ -68,12 +72,60 @@ App = {
     // Render Account
     $('#account').html(App.account)
 
-    // Render Tasks
     await App.renderQuotations()
+    
+    
+    const showTasks = await App.quotationContract.showtasks()
+
+    const inputTask = $('#inputT')
+    const showTask = $('#showT')
+  
+    const inputQuotation = $('#inputQ')
+    const showQuoation = $('#showQ')
+    
+    if(showTasks == 0) {
+      inputQuotation.show()
+      showQuoation.show()
+      inputTask.hide()
+      showTask.hide()
+      
+    } else {
+        inputQuotation.hide()
+        showQuoation.hide()
+        inputTask.show()
+        showTask.show()
+    }
+
+    
+    
+  
+    
 
     // Update loading state
     App.setLoading(false)
+
+   
+
   },
+
+///////////////////////////////// END METAMASK INTEGRATION /////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////// QUOTATIONS /////////////////////////////////////
+
 
 createQuotation: async () => {
 	App.setLoading(true)
@@ -83,62 +135,174 @@ createQuotation: async () => {
 	var id = await App.quotationContract.quotationCount()
 	id = Number(id) + 1
 	var tasks = []
-  	const contentJSON = JSON.stringify({id, airplane, item, obs, tasks})
+  const contentJSON = JSON.stringify({id, airplane, item, obs, tasks})
 	await App.quotationContract.createQuotation(contentJSON)
 	window.location.reload()
 },
 
-renderQuotations: async () => {
-	// Load the total task count from the blockchain
-	const quotationCount = await App.quotationContract.quotationCount()
-	var table = document.getElementById("quotation-table")
-     // Render out each task with a new task template
-     for (var i = 1; i <= quotationCount; i++) {
-		// Fetch the task data from the blockchain
-		const quotation = await App.quotationContract.quotations(i)
-		var row = table.insertRow(i)
-		var id = row.insertCell(0);
-		var airplane = row.insertCell(1);
-		var item = row.insertCell(2);
-		var obs = row.insertCell(3);
-		var button = row.insertCell(4);
-		const content = JSON.parse(quotation[1])
 
-		id.innerHTML = `<a id="quoteID"> ${content.id}</a>`
-		airplane.innerHTML = content.airplane
-		item.innerHTML = content.item
-		obs.innerHTML = content.obs
-		button.innerHTML = '<button onclick=App.renderTasks() style="font-size: 10px;">Tasks</button>';
+
+
+renderQuotations: async () => {
+
+	const quotationCount = await App.quotationContract.quotationCount()
+	
+	var table = document.getElementById("quotation-table")
+ 
+     
+    for (var i = 1; i <= quotationCount; i++) {
+		
+      const quotation = await App.quotationContract.quotations(i)
+      var row = table.insertRow(i)
+      var id = row.insertCell(0);
+      var airplane = row.insertCell(1);
+      var item = row.insertCell(2);
+      var obs = row.insertCell(3);
+      var button = row.insertCell(4);
+      
+      
+      
+      
+      const content = JSON.parse(quotation[1])
+
+      id.innerHTML = `<a id="quoteID${content.id}"> ${content.id}</a>`
+      airplane.innerHTML = content.airplane
+      item.innerHTML = content.item
+      obs.innerHTML = content.obs
+      button.innerHTML = `<button onclick=App.showTasks(${content.id}) style="font-size: 10px;">Tasks</button>`;
+      
+        
+       
+       
     }
   },
 
-createTask: async (taskCount, QuotationId) => {
+  returnToQuotation: async () => {
+    const inputTask = $('#inputT')
+    const showTask = $('#showT')
+
+    const inputQuotation = $('#inputQ')
+    const showQuoation = $('#showQ')
+
+    inputQuotation.show()
+    showQuoation.show()
+    inputTask.hide()
+    showTask.hide()
+  },
+
+///////////////////////////////////// END QUOTATIONS /////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////// TASKS /////////////////////////////////////
+
+
+showTasks: async (id) => {
+
+  App.renderTasks(id);
+  console.log(id);
+  const inputTask = $('#inputT')
+  const showTask = $('#showT')
+  inputTask.show()
+  showTask.show()
+
+  const inputQuotation = $('#inputQ')
+  const showQuoation = $('#showQ')
+  inputQuotation.hide()
+  showQuoation.hide()
+},
+
+createTask: async (QuotationId) => {
   App.setLoading(true)
-  const taskCount = await App.quotationContract.taskCount() + 1;
-  const QuotationId = $('#quoteID').val();
+  var count = await App.quotationContract.taskCount();
+  count = Number(count) + 1
+  const QId = $('#quoteID').val();
   const desc = $('#taskDescription').val();
-  const its_priceable = $('#priceable').val();
-  const price = 0;
-  if(priceable) {
-    price = $('#price').val();
-  } else {
-    price = -1;
-  }
+  const price = $('#price').val();;
+  const date = new Date()
 
   var task = {
-    id: taskCount, 
+    id: count, 
     description: desc,
-    priceable: its_priceable,
     priceUSD: price,
-    QuotationId,
+    userID: web3.eth.accounts[0],
+    dateModified: date,
+    QId,
   };
 
   const contentJSON = JSON.stringify(task);
   await App.quotationContract.createTask(contentJSON);
-  window.location.reload();
+  showtasks = 1
+  window.location.reload()
+  
+  
 },
 
+renderTasks: async () => {
+  const taskCount = await App.quotationContract.taskCount()
+	
+	var table = document.getElementById("task-table")
+ 
+     // Render out each task with a new task template
+     for (var i = 1; i <= taskCount; i++) {
+		// Fetch the task data from the blockchain
+    const task = await App.quotationContract.tasks(i)
+    
+		var row = table.insertRow(i)
+		var id = row.insertCell(0);
+		var desc = row.insertCell(1);
+		var price = row.insertCell(2);
+    var button = row.insertCell(3);
+    var account = row.insertCell(4);
+    var data = row.insertCell(5);
+		
+		
+		
+		const content = JSON.parse(task[1])
+		id.innerHTML = `<a id="taskID"> ${content.id}</a>`
+		desc.innerHTML = content.description
+		price.innerHTML = content.priceUSD
+    button.innerHTML = '<input type="radio" id="completed">'
+    account.innerHTML = content.userID
+    data.innerHTML = content.dateModified
+    }
+    
+
+},
+
+
+
 ///////////////////////////////////// END TASKS /////////////////////////////////////
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   toggleCompleted: async (e) => {
     App.setLoading(true)
